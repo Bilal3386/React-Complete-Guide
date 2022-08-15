b1bdc95c318ca22c1f1edec44d05573baf6741e8
mohdbilal <mohdbilal3386@gmail.com>
2022-08-15 17:38:52 +0530

    adding filter to Expneses filter

--- src/App.js
+++ src/App.js
@@ -1,42 +1,48 @@
-import Expenses, {useState} from "./components/Expenses/Expenses";
+import React, {useState} from "react";
+import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/newExpanse/NewExpense";
 
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    locationOfExpenditure: "in a mall",
+  },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    locationOfExpenditure: "in a car petrol",
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    locationOfExpenditure: "in a theater",
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    locationOfExpenditure: "in a shopping",
+  },
+];
+
 const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
   
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      locationOfExpenditure: "in a mall",
-    },
-    {
-      id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
-      locationOfExpenditure: "in a car petrol",
-    },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-      locationOfExpenditure: "in a theater",
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      locationOfExpenditure: "in a shopping",
-    },
-  ];
+  
  
   const addExpenseHandler = (expense) =>
   {
-    expenses.push({expense});
+    setExpenses(prevExpense => {
+      return [expense, ...prevExpense]
+    });
   }
   return (
     <div>
